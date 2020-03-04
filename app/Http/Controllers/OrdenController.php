63e78d9c2fa09aa67714e2a238f0703f09d0d2d7
Franco Reynoso <reynoso.franco@gmail.com>
2020-03-03 20:10:21 -0600

    Dos tablas

--- app/Http/Controllers/OrdenController.php
+++ app/Http/Controllers/OrdenController.php
@@ -11,7 +11,11 @@ use App\Helpers\JwtAuth;
 class OrdenController extends Controller
 {
     public function __construct() {
-		$this->middleware('api.auth', ['except' => ['index','show']]);
+		$this->middleware('api.auth', ['except' => ['index',
+			'show', 
+			'getOrdenByEstatus',
+			'getOdenByUsuario',
+		]]);
 	}
 
     public function index() {
@@ -54,9 +58,7 @@ class OrdenController extends Controller
 
     	if(!empty($params_array)) {
     		// Conseguir el usuario identificado
-    		$jwtAuth = new JwtAuth();
-    		$token = $request->header('Authorization', null);
-    		$user = $jwtAuth->checkToken($token, true);
+    		$user = $this->getIdentity($request);
 
     		//  Validar los datos
     		$validate = \Validator::make($params_array, [
@@ -102,5 +104,103 @@ class OrdenController extends Controller
     	}
 
     	return response()->json($data, $data['code']);
+    }
+
+    public function update($id, Request $request) {
+    	// Recoger los datos por POST
+    	$json = $request->input('json', null);
+    	$params_array = json_decode($json, true);
+
+    	if(!empty($params_array)) {
+    		// Validar datos
+    		$validate = \Validator::make($params_array, [
+    			'monto'		=> 'required',
+    			'estatus'	=> 'required'
+    		]);
+
+    		if($validate->fails()) {
+    			$data = array(
+    				'code'		=> 400,
+    				'status'	=> 'error',
+    				'message'	=> 'Datos erroneos',
+    			);
+    		} else {
+    			//Eliminar lo que no queremos actualizar
+    			unset($params_array['id']);
+    			unset($params_array['created_at']);
+    			unset($params_array['create_by']);
+
+    			$orden = Orden::where('id', $id)->update($params_array);
+
+    			$orden_o = Orden::find($id);
+
+    			$data = array(
+    				'code'		=> 200,
+    				'status'	=> 'success',
+    				'orden'		=>	$orden_o,
+    				'changes'	=> $params_array,
+    			);
+    		}
+    	} else {
+    		$data = array(
+    			'code'		=> 400,
+    			'status'	=> 'error',
+    			'message'	=> 'Datos erroneos',
+    		);
+    	}
+
+    	return response()->json($data, $data['code']);
+    }
+
+    public function destroy($id, Request $request) {
+    	// Conseguir el registro
+    	$orden = Orden::find($id);
+
+    	if(!empty($orden)) {
+    		// Borarlo
+    		$orden->delete();
+
+    		// Devolver algo
+    		$data = array(
+    			'code'		=> 200,
+    			'status'	=> 'success',
+    			'orden'		=> $orden
+    		);
+    	} else {
+    		$data = array(
+    			'code'		=> 400,
+    			'status'	=> 'error',
+    			'message'	=> 'La orden no existe',
+    		);
+    	}
+
+    	return response()->json($data, $data['code']);
+    }
+
+    private function getIdentity($request){
+    	// Conseguir el usuario identificado
+    	$jwtAuth = new JwtAuth();
+    	$token = $request->header('Authorization', null);
+   		$user = $jwtAuth->checkToken($token, true);
+
+   		return $user;
+    }
+
+    public function getOrdenByEstatus($id) {
+    	$ordenes = Orden::where('estatus', $id)->get();
+
+    	return response()->json([
+    		'status'	=> 'success',
+    		'ordenes'	=> $ordenes,
+    	], 200);
+    }
+
+    public function getOrdenByUsuario($id) {
+    	$ordenes = Orden::where('create_by', $id)->get();
+
+    	return response()->json([
+    		'status'	=> 'success',
+    		'ordenes'	=> $ordenes,
+    	], 200);
     }
 }
